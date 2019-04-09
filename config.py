f4454db2300dae1445b3b034dfbae6f99051c6f5
sonny-zhang <sonny.zhang@foxmail.com>
2019-04-09 09:55:15 +0800

    product environment add Error log that send its to ADMIN email

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'https://github.com/sonny-zhang'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    FLASK_MAIL_SUBJECT_PREFIX = '[Sonny]'
+    FLASK_MAIL_SUBJECT_PREFIX = '[Blog]'
     FLASK_ADMIN = os.environ.get('FLASK_ADMIN') or ''
     MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.qq.com')
     MAIL_PORT = os.environ.get('MAIL_PORT', '465')
@@ -54,6 +54,29 @@ class Testing(Config):
 class Product(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + \
                               os.path.join(basedir, 'data.sqlite')
+
+    @classmethod
+    def init_app(cls, app):
+        Config.init_app(app)
+
+        # email errors to the administrators
+        import logging
+        from logging.handlers import SMTPHandler
+        credentials = None
+        secure = None
+        if getattr(cls, 'MAIL_USERNAME', None) is not None:
+            credentials = (cls.MAIL_USERNAME, cls.MAIL_PASSWORD)
+            if getattr(cls, 'MAIL_USE_SSL', None):
+                secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(cls.MAIL_SERVER, cls.MAIL_PORT),
+            fromaddr=cls.FLASK_MAIL_SENDER,
+            toaddrs=[cls.FLASK_ADMIN],
+            subject=cls.FLASK_MAIL_SUBJECT_PREFIX + ' Application Error',
+            credentials=credentials,
+            secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
 
 
 config = {
